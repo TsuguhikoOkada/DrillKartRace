b71c03ceb3718f9061afaf4437dfabb80d129756
NaritaShosei <onigirikun7171@gmail.com>
2025-02-02 14:37:12 +0900

    [add]タイマー部分の根幹の作成

---
+++ DrillKart/Assets/Narita/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameManager _gameManager;
+    [SerializeField] TextMeshProUGUI _timerText;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timerText.text = $"{((int)(_gameManager.Timer / 60)).ToString("00")}<size=50>M</size> " +
+            $"{((int)_gameManager.Timer % 60).ToString("00")}<size=50>S</size>";
+    }
+}
