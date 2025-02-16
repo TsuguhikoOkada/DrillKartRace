bc5755c7d20371af926b9e92e24c933681de9719
NaritaShosei <onigirikun7171@gmail.com>
2025-02-16 16:55:05 +0900

    [add]とりあえずカートを選ぶ処理

---
+++ DrillKart/Assets/Narita/Scripts/KartSelector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartSelector : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void KartSelect(GameObject kart)
+    {
+        Debug.Log(kart.name + "‚ª‘I‚Î‚ê‚Ü‚µ‚½");
+    }
+}
